4f21c5e2a554fb99db69581b7ee60c49332f1cac
Ubuntu <ubuntu@ip-172-31-42-84.eu-north-1.compute.internal>
2025-03-29 12:05:25 +0000

    commiting all files from jenkins job

---
+++ pullAlljobFromJenks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "===========================Copying all jobs from /var/lib/jenkins/jobs/ =============================================="
+sudo cp -r /var/lib/jenkins/jobs/* /home/ubuntu/jenkins_backup/jenkins_repo/
+ls -lt
+
+echo "====================Adding all files to git ==========================="
+git add * 
+ echo "==============================committig all files to git=========================================="
+
+ git commit -m "commiting all files from jenkins job $date"
