65f16967876396f9aa0cbdaa257fb96d4fc5ed73
root <root@ip-172-31-42-84.eu-north-1.compute.internal>
2025-04-02 13:19:38 +0000

    jenkins file added

--- pullAlljobFromJenks.sh
+++ pullAlljobFromJenks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "===========================Copying all jobs from /var/lib/jenkins/jobs/ =============================================="
-sudo cp -r /var/lib/jenkins/jobs/* /home/ubuntu/jenkins_backup/jenkins_repo/jenkins_repo
+sudo cp -r /var/lib/jenkins/  /home/ubuntu/jenkins_backup/jenkins_repo/jenkins_repo
 ls -lt
 
 echo "====================Adding all files to git ==========================="
